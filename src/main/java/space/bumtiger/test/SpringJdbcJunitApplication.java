b455f69c30331d1b0b93583e3ed707b412584c04
osparking <jbpark03@gmail.com>
2023-04-11 06:51:26 +0900

    mockmvc jdbc repo' layer test bed inited

---
+++ src/main/java/space/bumtiger/test/SpringJdbcJunitApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/test/SpringJdbcJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

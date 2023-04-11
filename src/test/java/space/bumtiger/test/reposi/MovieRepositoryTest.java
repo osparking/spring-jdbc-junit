3535fbc62f03a2f09448ae0c9b5d0af373b4103a
osparking <jbpark03@gmail.com>
2023-04-11 13:15:03 +0900

    test if movie is saved well - v.1

--- src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
+++ src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
@@ -1,8 +1,33 @@
 package space.bumtiger.test.reposi;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+
+import space.bumtiger.test.model.Movie;
 
 @DataJdbcTest
 public class MovieRepositoryTest {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Test
+	void save() {
+		// Arrange
+		var movie = new Movie();
+		movie.setName("율돌목");
+		movie.setGenera("전쟁");
+		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+		
+		// Act
+		var savedMovie = repository.save(movie);
+		
+		// Assert
+		assertNotNull(savedMovie);		
+	}
 
 }

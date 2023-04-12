30a6498374aecdfe29482e5f0cd8d1e6ed350aba
osparking <jbpark03@gmail.com>
2023-04-13 00:57:37 +0900

    movie saved in home() method

--- src/main/java/space/bumtiger/test/controller/HomeController.java
+++ src/main/java/space/bumtiger/test/controller/HomeController.java
@@ -1,12 +1,29 @@
 package space.bumtiger.test.controller;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import space.bumtiger.test.model.Movie;
+import space.bumtiger.test.reposi.MovieRepository;
 
 @RestController
 public class HomeController {
 	@GetMapping("/")
 	public String home() {
+		var movie = new Movie();
+		movie.setName("율돌목");
+		movie.setGenera("전쟁");
+		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+		repository.save(movie);
+		
 		return "Greeting from spring-jdbc-junit";
 	}
+	
+	@Autowired
+	private MovieRepository repository;
+
 }

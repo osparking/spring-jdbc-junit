a31e8c54ae0cd88fd033a0def35531658bc3cfa3
osparking <jbpark03@gmail.com>
2023-04-12 11:06:51 +0900

    new movie id assigned

--- src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
+++ src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
@@ -1,5 +1,6 @@
 package space.bumtiger.test.reposi;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
@@ -13,10 +14,10 @@ import space.bumtiger.test.model.Movie;
 
 @DataJdbcTest
 public class MovieRepositoryTest {
-	
+
 	@Autowired
 	private MovieRepository repository;
-	
+
 	@Test
 	void save() {
 		// Arrange
@@ -24,12 +25,13 @@ public class MovieRepositoryTest {
 		movie.setName("율돌목");
 		movie.setGenera("전쟁");
 		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
-		
+
 		// Act
 		var savedMovie = repository.save(movie);
-		
+
 		// Assert
-		assertNotNull(savedMovie);		
+		assertNotNull(savedMovie);
+		assertThat(savedMovie.getId()).isNotEqualTo(null);
 	}
 
 }

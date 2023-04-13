97cb5ddba6f16b6aa8fa86289c22b51d55005c03
osparking <jbpark03@gmail.com>
2023-04-13 10:46:47 +0900

    test case for find by ID

--- src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
+++ src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +21,28 @@ public class MovieRepositoryTest {
 	private MovieRepository repository;
 
 	@Test
+	@DisplayName("영화가 ID로 찾아짐")
+	void findMovieById() {
+		// Arrange
+		var movie = new Movie();
+		movie.setName("율돌목");
+		String genera = "전쟁";
+		movie.setGenera(genera);
+		var relDate = LocalDate.of(2009, Month.NOVEMBER, 10);
+		movie.setReleaseDate(relDate);
+		var savedMovie = repository.save(movie);
+		
+		// Act
+		var foundMovie = repository.findById(savedMovie.getId());
+		
+		// Assert
+		assertNotNull(foundMovie);
+		Movie fMovie = foundMovie.isPresent() ? foundMovie.get() : null;
+		assertThat(fMovie.getGenera()).isEqualTo(genera);
+		assertThat(fMovie.getReleaseDate()).isAfter(relDate.minusDays(1L));
+	}
+
+	@Test
 	@DisplayName("영화 2 건이 읽혀질 것임")
 	void fetchAll() {
 		// Arrange
@@ -36,23 +57,24 @@ public class MovieRepositoryTest {
 		movie.setGenera("전쟁");
 		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
 		repository.save(movie);
-		
+
 		// act
 		Iterable<Movie> movies = repository.findAll();
-		
-		// Assert
+
+		// Assert 1
 		assertNotNull(movies);
-		
+
 		var iter = movies.iterator();
 		int count = 0;
 		while (iter.hasNext()) {
 			iter.next();
 			count++;
 		}
-		
+
+		// Assert 2
 		assertThat(count).isEqualTo(2);
 	}
-	
+
 	@Test
 	@DisplayName("영화가 DB에 저장됨")
 	void save() {

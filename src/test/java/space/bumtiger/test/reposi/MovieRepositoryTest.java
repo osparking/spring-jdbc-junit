9205e3db2226823fc6d859f7c253e0e5a29162e8
osparking <jbpark03@gmail.com>
2023-04-13 09:54:40 +0900

    find all movies in DB table

--- src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
+++ src/test/java/space/bumtiger/test/reposi/MovieRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,38 @@ public class MovieRepositoryTest {
 	@Autowired
 	private MovieRepository repository;
 
+	@Test
+	@DisplayName("영화 2 건이 읽혀질 것임")
+	void fetchAll() {
+		// Arrange
+		var movie = new Movie();
+		movie.setName("율돌목");
+		movie.setGenera("전쟁");
+		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+		repository.save(movie);
+
+		movie = new Movie();
+		movie.setName("율돌목");
+		movie.setGenera("전쟁");
+		movie.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+		repository.save(movie);
+		
+		// act
+		Iterable<Movie> movies = repository.findAll();
+		
+		// Assert
+		assertNotNull(movies);
+		
+		var iter = movies.iterator();
+		int count = 0;
+		while (iter.hasNext()) {
+			iter.next();
+			count++;
+		}
+		
+		assertThat(count).isEqualTo(2);
+	}
+	
 	@Test
 	@DisplayName("영화가 DB에 저장됨")
 	void save() {

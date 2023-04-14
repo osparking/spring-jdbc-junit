40c27298aabefcc54be2d607f1fd9b158fca39f6
osparking <jbpark03@gmail.com>
2023-04-14 14:26:34 +0900

    service class for JUnit test

---
+++ src/main/java/space/bumtiger/test/service/MovieService.java
@@ -0,0 +1,41 @@
+package space.bumtiger.test.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import space.bumtiger.test.model.Movie;
+import space.bumtiger.test.reposi.MovieRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+
+	private final MovieRepository movieRepository;
+
+	public Movie save(Movie movie) {
+		return movieRepository.save(movie);
+	}
+
+	public Iterable<Movie> getAllMovies() {
+		return movieRepository.findAll();
+	}
+
+	public Movie getMovieById(Long id) {
+		return movieRepository.findById(id).orElseThrow(
+				() -> new RuntimeException("찾으려 했던 영와 ID: " + id));
+	}
+
+	public Movie updateMovie(Movie movie, Long id) {
+		Movie existingMovie = movieRepository.findById(id).get();
+		existingMovie.setGenera(movie.getGenera());
+		existingMovie.setName(movie.getName());
+		existingMovie.setReleaseDate(movie.getReleaseDate());
+		return movieRepository.save(existingMovie);
+	}
+
+	public void deleteMovie(Long id) {
+		Movie existingMovie = movieRepository.findById(id).get();
+		movieRepository.delete(existingMovie);
+
+	}
+}

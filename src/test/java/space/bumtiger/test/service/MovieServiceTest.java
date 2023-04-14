a2a84853b5d99422d0e85733994af8de0fe54c37
osparking <jbpark03@gmail.com>
2023-04-14 15:38:26 +0900

    mcreated movie object, save returns it

--- src/test/java/space/bumtiger/test/service/MovieServiceTest.java
+++ src/test/java/space/bumtiger/test/service/MovieServiceTest.java
@@ -1,12 +1,14 @@
 package space.bumtiger.test.service;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.time.LocalDate;
+import java.time.Month;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import space.bumtiger.test.model.Movie;
 import space.bumtiger.test.reposi.MovieRepository;
 
 class MovieServiceTest {
@@ -20,7 +22,12 @@ class MovieServiceTest {
 	@Test
 	@DisplayName("영화 객체를 DB 에 저장한다")
 	void save() {
-		fail("Not yet implemented");
+		var yulDolMok = new Movie();
+		yulDolMok.setId(2L);
+		yulDolMok.setName("율돌목");
+		yulDolMok.setGenera("역사");
+		yulDolMok.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+		
 	}
 
 }

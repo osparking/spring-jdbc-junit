eed3e0dca2b3d0b8d85f8c8ba60c72b3b701b116
osparking <jbpark03@gmail.com>
2023-04-16 11:24:13 +0900

    members for controller method test

--- src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
+++ src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
@@ -1,9 +1,17 @@
 package space.bumtiger.test.controller;
 
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import space.bumtiger.test.service.MovieService;
 
 class MovieControllerTest {
 
 	private MovieService service;
+	
+	private MockMvc mockMvc;
+	
+	private ObjectMapper objectMapper;
 
 }

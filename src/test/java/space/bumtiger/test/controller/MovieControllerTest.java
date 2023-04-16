09547920d4086881a281137e9b4f3e6541be7b97
osparking <jbpark03@gmail.com>
2023-04-16 11:35:51 +0900

    three annotations added

--- src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
+++ src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
@@ -1,17 +1,22 @@
 package space.bumtiger.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import space.bumtiger.test.service.MovieService;
 
+@WebMvcTest
 class MovieControllerTest {
 
 	private MovieService service;
 	
+	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
 	private ObjectMapper objectMapper;
 
 }

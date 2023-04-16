8f3f3c07479fbd65d5fcdd1d932fc783d3e0a3d8
osparking <jbpark03@gmail.com>
2023-04-16 13:40:05 +0900

    case for controller.create()

--- src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
+++ src/test/java/space/bumtiger/test/controller/MovieControllerTest.java
@@ -1,12 +1,27 @@
 package space.bumtiger.test.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import space.bumtiger.test.model.Movie;
 import space.bumtiger.test.service.MovieService;
 
 @WebMvcTest
@@ -14,11 +29,45 @@ class MovieControllerTest {
 
 	@MockBean
 	private MovieService service;
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	private Movie yulDolMok;
+	private Movie chunHyangJeon;
+
+	@BeforeEach
+	void makeMovies() {
+		yulDolMok = new Movie();
+		yulDolMok.setId(1L);
+		yulDolMok.setName("율돌목");
+		yulDolMok.setGenera("역사");
+		yulDolMok.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+
+		chunHyangJeon = new Movie();
+		chunHyangJeon.setName("춘향전");
+		chunHyangJeon.setGenera("로맨스");
+		chunHyangJeon.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+	}
+
+	@Test
+	@DisplayName("영화 생성하면 DB에 저장된다.")
+	void createTest() throws Exception {
+		// arrange
+		// @formatter:off -- act and expect(=assert)
+		when(service.save(any(Movie.class))).thenReturn(yulDolMok);
+		
+		mockMvc.perform
+		(post("/movies")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(yulDolMok))
+		).andExpect(status().isCreated())
+		 .andExpect(jsonPath("$.name", is(yulDolMok.getName())))
+		 .andExpect(jsonPath("$.genera", is(yulDolMok.getGenera())))
+		 .andExpect(jsonPath("$.releaseDate", is(yulDolMok.getReleaseDate())));
+		// @formatter:on
+	}
 }

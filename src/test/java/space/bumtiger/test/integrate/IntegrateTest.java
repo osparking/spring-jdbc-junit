a2bc22f27f5c64da69eb1c61dbab7a0563eb6f51
osparking <jbpark03@gmail.com>
2023-04-18 20:33:01 +0900

    tests if movie creates well

--- src/test/java/space/bumtiger/test/integrate/IntegrateTest.java
+++ src/test/java/space/bumtiger/test/integrate/IntegrateTest.java
@@ -1,13 +1,22 @@
 package space.bumtiger.test.integrate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
+import space.bumtiger.test.model.Movie;
 import space.bumtiger.test.reposi.MovieRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,6 +27,21 @@ public class IntegrateTest {
 	private static RestTemplate restTemplate;
 	@Autowired
 	private MovieRepository repository;
+
+	@Test
+	@DisplayName("집적 시험 - 영화 저장")
+	void shouldCreateMovieTest() {
+		Movie yulDolMok = new Movie();
+		yulDolMok.setName("율돌목");
+		yulDolMok.setGenera("역사");
+		yulDolMok.setReleaseDate(LocalDate.of(2009, Month.NOVEMBER, 10));
+
+		Movie savedMovie = restTemplate.postForObject(baseUrl, yulDolMok,
+				Movie.class);
+		
+		assertNotNull(savedMovie);
+		assertThat(savedMovie.getId()).isNotNull();
+	}
 
 	@BeforeAll
 	public static void init() {

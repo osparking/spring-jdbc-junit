c0380096ed6b8c64b6f62f4d32605e680ecc10c2
osparking <jbpark03@gmail.com>
2023-04-18 20:23:03 +0900

    integration test class added

---
+++ src/test/java/space/bumtiger/test/integrate/IntegrateTest.java
@@ -0,0 +1,9 @@
+package space.bumtiger.test.integrate;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrateTest {
+
+}
